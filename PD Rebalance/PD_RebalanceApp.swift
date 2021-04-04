6d32fa15b7c58aa130164d6fda26e5ecd815d2a4
George Breen <george.breen@rabbithilsolutions.com>
2021-04-04 16:16:51 -0400

    Initial Commit

---
+++ PD Rebalance/PD_RebalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PD_RebalanceApp.swift
+//  PD Rebalance
+//
+//  Created by George Breen on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct PD_RebalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PD RebalanceTests/PD_RebalanceTests.swift
@@ -0,0 +1,33 @@
+//
+//  PD_RebalanceTests.swift
+//  PD RebalanceTests
+//
+//  Created by George Breen on 4/4/21.
+//
+
+import XCTest
+@testable import PD_Rebalance
+
+class PD_RebalanceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
